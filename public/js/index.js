32680797154399f1118247f54deb8cf348556459
Ilija Ilievski <ilija139@gmail.com>
2016-09-29 10:14:15 +0800

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,7 @@
             $('#formContainer').html('<h3>Thank you!</h3>');
             return false;
         });
-        var target_date = new Date("Sep 30, 2016").getTime();
+        var target_date = new Date("Oct 30, 2016").getTime();
          
         var days, hours, minutes, seconds, countdown = document.getElementById("countdown");
          
